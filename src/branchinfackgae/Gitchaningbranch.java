75f5738c6a050b555184e9354dc73c0c5a3564cd
Sergio Palomino Guevara <sergiopalominoguevara@sergios-mbp.mynetworksettings.com>
2022-11-08 21:37:47 -0500

    seond update

--- src/branchinfackgae/Gitchaningbranch.java
+++ src/branchinfackgae/Gitchaningbranch.java
@@ -3,5 +3,13 @@ package branchinfackgae;
 public class Gitchaningbranch {
 public static void main(String[] args) {
 	System.out.println("hola mi name is yolo");
+	System.out.println("ajajja");
+	System.out.println("javsfjajfhshavsf");
 }
+
+public void test111(){
+	System.out.println("jhjhgjhgdsgsdgg");
+}
+
+
 }
